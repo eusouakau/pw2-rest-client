384a709ea2a22c11ed5fef79cc677f99c8ea7db3
Kauana Henriques <karoltvbl@gmail.com>
2022-04-01 00:01:25 -0300

    test

--- servicea/src/main/java/dev/rpmhub/ServiceA.java
+++ servicea/src/main/java/dev/rpmhub/ServiceA.java
@@ -15,11 +15,14 @@
  */
 package dev.rpmhub;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 @Path("/servicea")
 public class ServiceA {
@@ -27,6 +30,10 @@ public class ServiceA {
     // TODO
     // Por meio de injeção de dependência,
     // instancie o Rest Cliente para o serviço B
+    @Inject
+    @RestClient
+
+    ServiceB service;
 
     @GET
     @Path("/person/{name}")
@@ -34,7 +41,7 @@ public class ServiceA {
     public Person getPerson(@PathParam("name") String name){
         // TODO
         // Complete o método
-        return null;
+        return service.getPerson(name);
     }
 
 }
